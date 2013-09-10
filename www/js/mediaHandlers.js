bdb1d99d055b2e972cb1416b7ee608edcbf8454a
izraylevych-is <izraylevych-is@comp10.(none)>
2013-09-10 11:49:20 +0300

    mp3

--- www/js/mediaHandlers.js
+++ www/js/mediaHandlers.js
@@ -12,9 +12,9 @@ var recTime = 0;
 
 // for recording: do not specify any directory
 var mediaFileFullName = null; 
-var mediaRecFile = "myRecording100.wav";
+//var mediaRecFile = "myRecording100.wav";
 //var mediaRecFile = "myRecording100.amr";
-//var mediaRecFile = "myRecording100.mp3";
+var mediaRecFile = "myRecording100.mp3";
 var checkFileOnly = false;
 var mediaFileExist = false;
 var myMediaState = {start: 1, 
@@ -210,6 +210,8 @@ function playMusic() {
 // Pause audio
 //
 function pauseMusic() {
+    console.log("my_audio")
+    console.log(my_audio)
     if (my_audio) {
         my_audio.pause();
         document.getElementById('PlayStatusID').innerHTML = "Status: paused";
