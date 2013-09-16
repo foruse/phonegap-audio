18650683fe7c744ad58906dc9c4a65510119d58b
izraylevych-is <izraylevych-is@comp10.(none)>
2013-09-16 11:14:27 +0300

    test3

--- www/js/media.js
+++ www/js/media.js
@@ -87,6 +87,7 @@ Voice_message = {
     play    :   function(){
         var _this =this;
         console.log(_this.audio);
+        console.log("_this.audio");
         if (this.audio == null) {
 //            return false;
             _this.audio = new Media(_this.file_name, _this.recordSuccess, _this.recordError);
